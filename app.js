258f12af6533f38018c55b924dd24cc701feaf95
Henning <henning.sternkicker@de.ibm.com>
2019-11-20 14:58:50 +0100

    Test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Hennings Tekton!");
+  res.send("Hello from SVA  Hennings Tekton!");
 });
  
 module.exports.app = app;
